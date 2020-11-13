9e7d622ce86ecb2c99c32e692985e12a26ef5482
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-12 21:19:48 -0600

    Adds additional styling to main app container and post container along with dummy test data.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import "./App.css";
+import Post from "./Post";
 
 function App() {
   return (
     <div className="App">
-      <h1>Instagram Clone w/ React</h1>
       <div className="app__header">
         <img
           className="app__headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-          alt="Instaagram Logo"
+          alt="Instagram Logo"
         />
       </div>
 
-      {/* posts */}
-      {/* posts */}
-      {/* posts */}
+      <Post
+        username="kanderson425"
+        imageUrl="https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg"
+        caption="Wow it works"
+      />
+      <Post
+        username="csmith52"
+        imageUrl="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=640:*"
+        caption="I like puppies"
+      />
+      <Post
+        username="ssmith163"
+        imageUrl="https://icatcare.org/app/uploads/2018/06/Layer-1704-1200x630.jpg"
+        caption="I like cats"
+      />
+      <Post
+        username="taletell420"
+        imageUrl="https://i1.sndcdn.com/artworks-000268545731-9tescq-t500x500.jpg"
+        caption="Wow, I'm so high!"
+      />
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
+
+function Post({ username, caption, imageUrl }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="Kyle Anderson"
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>{username}</h3>
+      </div>
+      <img className="post__image" src={imageUrl} alt="React Logo" />
+      <h4 className="post__text">
+        <strong>{username}</strong> {caption}
+      </h4>
+    </div>
+  );
+}
+
+export default Post;
