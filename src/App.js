5abefe3294c1529423bdc3150430c014b668da49
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-12 20:29:42 -0600

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Instagram Clone w/ React</h1>
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="Instaagram Logo"
+        />
+      </div>
+
+      {/* posts */}
+      {/* posts */}
+      {/* posts */}
+    </div>
+  );
+}
+
+export default App;
