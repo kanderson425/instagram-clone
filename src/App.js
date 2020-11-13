d3bcd01cf251399cee09ecc4020c2662f25cd2ab
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-12 21:45:20 -0600

    Adds the posts.map call to iterate through the list of user data and generate user-specific post components.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "kanderson425",
+      imageUrl:
+        "https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg",
+      caption: "Wow it works",
+    },
+    {
+      username: "csmith52",
+      imageUrl:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=640:*",
+      caption: "I like puppies",
+    },
+    {
+      username: "ssmith163",
+      imageUrl:
+        "https://icatcare.org/app/uploads/2018/06/Layer-1704-1200x630.jpg",
+      caption: "I like cats",
+    },
+    {
+      username: "taletell420",
+      imageUrl:
+        "https://i1.sndcdn.com/artworks-000268545731-9tescq-t500x500.jpg",
+      caption: "Wow, I'm so high!",
+    },
+  ]);
+
   return (
     <div className="App">
       <div className="app__header">
@@ -11,27 +39,13 @@ function App() {
           alt="Instagram Logo"
         />
       </div>
-
-      <Post
-        username="kanderson425"
-        imageUrl="https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg"
-        caption="Wow it works"
-      />
-      <Post
-        username="csmith52"
-        imageUrl="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=640:*"
-        caption="I like puppies"
-      />
-      <Post
-        username="ssmith163"
-        imageUrl="https://icatcare.org/app/uploads/2018/06/Layer-1704-1200x630.jpg"
-        caption="I like cats"
-      />
-      <Post
-        username="taletell420"
-        imageUrl="https://i1.sndcdn.com/artworks-000268545731-9tescq-t500x500.jpg"
-        caption="Wow, I'm so high!"
-      />
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
