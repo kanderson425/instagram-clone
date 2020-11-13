ccf44cf2aa983335a76efcc39a928bf52e6270e2
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-12 22:39:01 -0600

    Finalizes the Firebase configuration and sets the real-time database connection up.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
+import { db } from "./firebase";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "kanderson425",
-      imageUrl:
-        "https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg",
-      caption: "Wow it works",
-    },
-    {
-      username: "csmith52",
-      imageUrl:
-        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=640:*",
-      caption: "I like puppies",
-    },
-    {
-      username: "ssmith163",
-      imageUrl:
-        "https://icatcare.org/app/uploads/2018/06/Layer-1704-1200x630.jpg",
-      caption: "I like cats",
-    },
-    {
-      username: "taletell420",
-      imageUrl:
-        "https://i1.sndcdn.com/artworks-000268545731-9tescq-t500x500.jpg",
-      caption: "Wow, I'm so high!",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
 
   return (
     <div className="App">
